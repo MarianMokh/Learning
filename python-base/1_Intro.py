3eccec7dde4365488733bb2b309b376c344098bc
Marian Mokh <marianmokh@proton.me>
2026-07-09 14:46:29 +0300

    feat: add python lesson 1

---
+++ python-base/1_Intro.py
@@ -0,0 +1,18 @@
+# ===== 1. Intro =====
+
+# Call a built-in fuction
+print("Hello, World!")
+
+# () - parentheses, "" - quotes, * - asterisk
+print("* " * 5)
+
+# \ - backslash
+# Escape sequences: \n, \", \\ ...
+print("\tHi!")
+
+# Declare a variable
+x = 1
+
+# Expression (5 + 2) - piece of code that produces a value
+# PEP - Python Enhancement Proposals
+# PEP8 - coding convention
