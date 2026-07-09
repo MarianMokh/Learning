254817218439a629454a7ef29c40f37541c2b1ca
Marian Mokh <marianmokh@proton.me>
2026-07-09 15:55:01 +0300

    feat: add python lesson 3

---
+++ python-base/3_numbers_and_types.py
@@ -0,0 +1,39 @@
+print("\n# ===== 1. Numbers =====\n")
+
+# Import a module
+import math
+
+# Get an integer, // - double slash
+print(f"10 // 3 = {10 // 3}")
+
+# Remainder of division, % - percent
+print(f"10 % 3 = {10 % 3}")
+
+# Rounding a number
+print(f"\nround(2.7): {round(2.7)}")
+
+# Ceiling of a number
+print(f"math.ceil(2.2): {math.ceil(2.2)}")
+
+# Absolute value
+print(f"abs(-5): {abs(-5)}")
+
+
+print("\n# ===== 2. Type conversion =====\n")
+
+# Get input from the user
+x = input("x: ")
+
+# Built-in function that returns type of an object
+print(f"Type of x: {type(x)}")
+
+# Convert to an integer
+x = int(x) + 2
+print(f"\nx + 2 = {x}")
+
+# augmented assignment operator
+x += 5
+print(f"x += 5: {x}")
+
+# falsey values: "", 0, None
+# anything that is not falsey is considered truthy
